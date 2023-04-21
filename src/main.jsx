ab782ddfae3c572a4d5dcacea85f2ed2e386aa37
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-21 21:57:31 +0530

    Created

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import FreeChoiceQuestion from '../components/multipe-choice'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FreeChoiceQuestion />
+  </React.StrictMode>
+);
