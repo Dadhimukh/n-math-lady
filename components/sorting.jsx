a8f7dffb3c41aba16b4bc66c5060fca02c8caa86
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-05-11 13:31:37 +0530

    Updated

--- components/sorting.jsx
+++ components/sorting.jsx
@@ -1,75 +1,105 @@
-   import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-   import { useState } from "react";
+import { useState } from "react";
+// Importing required components from the react-beautiful-dnd and react packages
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-   const userData = ["Red", "Yellow", "Blue", "Green", "Black"];
+// Creating an array of colors to be sorted
+const userData = ["Red", "Yellow", "Blue", "Green", "Black"];
 
-   const answer = ["Red", "Green", "Blue", "Black", "Yellow"];
+// Creating an array of correct color order
+const answer = ["Red", "Green", "Blue", "Black", "Yellow"];
 
-   export default function Sorting() {
-   const [users, setUsers] = useState(userData);
-   const [isCorrect, setIsCorrect] = useState(null);
+export default function Sorting() {
+  // Initializing state for users array and whether the sorting is correct or not
+  const [users, setUsers] = useState(userData);
+  const [isCorrect, setIsCorrect] = useState(null);
 
-   const handleDragEnd = (e) => {
-      if (!e.destination) return;
-      let tempData = Array.from(users);
-      let [source_data] = tempData.splice(e.source.index, 1);
-      tempData.splice(e.destination.index, 0, source_data);
-      setUsers(tempData);
-   };
+  // Function to handle the drag and drop of the colors
+  const handleDragEnd = (e) => {
+    // If the destination is empty, return
+    if (!e.destination) return;
+    // Create a copy of the users array
+    let tempData = Array.from(users);
+    // Remove the dragged element from the source index
+    let [source_data] = tempData.splice(e.source.index, 1);
+    // Insert the dragged element at the destination index
+    tempData.splice(e.destination.index, 0, source_data);
+    // Update the users state with the new order
+    setUsers(tempData);
+  };
 
-   const handleSubmit = (event) => {
-      event.preventDefault();
+  // Function to handle the form submission and check if the order is correct
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-      const isAllCorrect = users.every((ans, index) => ans === answer[index]);
+    // Check if all the colors are in the correct order
+    const isAllCorrect = users.every((ans, index) => ans === answer[index]);
 
-      setIsCorrect(isAllCorrect);
-   };
+    // Set the isCorrect state with the result
+    setIsCorrect(isAllCorrect);
+  };
 
-   return (
-      <div>
-         <span>Red -- </span>
-         <span>Green -- </span>
-         <span>Blue -- </span>
-         <span>Black -- </span>
-         <span>Yellow </span>
+  return (
+    <div>
+      <span>Red -- </span>
+      <span>Green -- </span>
+      <span>Blue -- </span>
+      <span>Black -- </span>
+      <span>Yellow </span>
 
-         <DragDropContext onDragEnd={handleDragEnd}>
-         <table>
-            <thead>
-               <tr>
-               <th />
-               <th>Arrange The Color</th>
-               </tr>
-            </thead>
-            <Droppable droppableId="droppable-1">
-               {(provider) => (
-               <tbody
-                  ref={provider.innerRef}
-                  {...provider.droppableProps}
-               >
-                  {users?.map((user, index) => (
-                     <Draggable key={user} draggableId={user} index={index}>
-                     {(provider) => (
-                        <tr
-                           {...provider.draggableProps}
-                           ref={provider.innerRef}
-                           {...provider.dragHandleProps}
+      {/* Wrap the sortable table in a DragDropContext component with handleDragEnd function as a prop */}
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <table>
+          <thead>
+            <tr>
+              <th>Arrange The Color</th>
+            </tr>
+          </thead>
+
+          {/* Wrap the table body in a Droppable component with droppableId prop set to "droppable-1" */}
+          <Droppable droppableId="droppable-1">
+            {/* Render the table body as a function that takes a 'provider' argument */}
+            {(provider) => (
+              <tbody
+                // Use the innerRef prop provided by Droppable for the table body reference
+                ref={provider.innerRef}
+                // Spread the droppableProps provided by Droppable onto the table body
+                {...provider.droppableProps}
+              >
+                {/* Map over the current order of colors and render each one as a draggable row */}
+                {users?.map((user, index) => (
+                  <Draggable key={user} draggableId={user} index={index}>
+                    {/* Render each draggable row as a function that takes a 'provider' argument */}
+                    {(provider) => (
+                      <tr
+                        {...provider.draggableProps}
+                        ref={provider.innerRef}
+                        {...provider.dragHandleProps}
+                      >
+                        <td
+                          style={{
+                            color: "red",
+                            textAlign: "center",
+                            width: 500,
+                            border: "1px dotted white",
+                          }}
                         >
-                           <td>{user}</td>
-                        </tr>
-                     )}
-                     </Draggable>
-                  ))}
-                  {provider.placeholder}
-               </tbody>
-               )}
-            </Droppable>
-         </table>
-         </DragDropContext>
-         <button onClick={handleSubmit}>Submit</button>
-         {isCorrect !== null && (
-         <p>{isCorrect ? "Correct!" : "Incorrect, try again."}</p>
-         )}
-      </div>
-   );
-   }
+                          {user}
+                        </td>
+                      </tr>
+                    )}
+                  </Draggable>
+                ))}
+                {provider.placeholder}
+              </tbody>
+            )}
+          </Droppable>
+        </table>
+      </DragDropContext>
+      <button onClick={handleSubmit}>Submit</button>
+      {isCorrect !== null && (
+        <p>{isCorrect ? "Correct!" : "Incorrect, try again."}</p>
+      )}
+      <hr />
+    </div>
+  );
+}
